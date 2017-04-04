eaa2be1d143d862b52a31da700e64336c49335e5
Miguel Hidalgo <hpmiguel@Miguels-MacBook-Pro.local>
2017-04-04 18:42:15 +0200

    dependecies installation janus updated

--- Janus/Backend/install_gateway.sh
+++ Janus/Backend/install_gateway.sh
@@ -1,9 +1,18 @@
 # Dependencies
+    
 sudo apt-get install libmicrohttpd-dev libjansson-dev libnice-dev \
-    libssl-dev libsrtp-dev libsofia-sip-ua-dev libglib2.0-dev \
-    libopus-dev libogg-dev pkg-config gengetopt libtool automake
-
-sudo apt-get install gupnp-igd-1.0
+	libssl-dev libsofia-sip-ua-dev libglib2.0-dev \
+	libopus-dev libogg-dev libcurl4-openssl-dev pkg-config gengetopt \
+	libtool automake gupnp-igd-1.0
+    
+# to install new version libsrtp-dev
+wget https://github.com/cisco/libsrtp/archive/v1.5.4.tar.gz
+tar xfv v1.5.4.tar.gz
+cd libsrtp-1.5.4
+./configure --prefix=/usr --enable-openssl
+make shared_library && sudo make install
+cd ..
+#sudo rm -rf libsrtp-1.5.4
 
 # Source
 git clone https://github.com/meetecho/janus-gateway.git
